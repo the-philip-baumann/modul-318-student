d79e3acfaae3e6f00b24eb9edad7b35c9df1f4a2
the-philip-baumann <philip.baumann@hispeed.ch>
2020-03-10 13:55:57 +0100

    Implementiert Anforderung 1

---
+++ TransportGUI/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TransportGUI {
+    class TransportException {
+        private string name;
+        private string message;
+        public TransportException (string name, string message) {
+            this.name = name;
+            this.message = message;
+            showTransportExceptionDialog();
+        }
+
+        public void showTransportExceptionDialog () {
+            MessageBox.Show(message, name, MessageBoxButton.OK);
+        }
+    }
+}
--- TransportGUI/MainWindow.xaml.cs
+++ TransportGUI/MainWindow.xaml.cs
@@ -12,14 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SwissTransport;
 
 namespace TransportGUI {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        Transport transport;
         public MainWindow() {
             InitializeComponent();
+            transport = new Transport();
+            
         }
+
+        private void searchStationByName (object sender, RoutedEventArgs e) {
+            if (textBoxSearchValue.Text.Length != 0) {
+                // Call webservice with the users query
+                string query = textBoxSearchValue.Text;
+                Stations station = transport.GetStations(query);
+                displayStationsOnListBox(station);
+            } else {
+                new TransportException("Textbox ist leer", "Geben sie eine Station an!");
+            }
+        }
+
+        private void displayStationsOnListBox (Stations stations) {
+            List<Station> listStations = stations.StationList;
+            // Check if response contains a result and is not empty
+            if (listStations.Count != 0) {
+                foreach (Station station in listStations) {
+                    listBoxResult.Items.Add(station.Name.ToString());
+                }
+            } else {
+                new TransportException("Keine Ergebnisse gefunden", "Für Ihre Suchanfrage wurde kein Ergebnis gefunden");
+            }
+        }
+       
     }
 }
